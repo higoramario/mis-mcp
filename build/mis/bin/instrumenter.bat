1ee8d7ffca0ce3529d85dacb8945b1b735e4dd48
higoramario <higoramario@usp.br>
2017-05-20 21:14:30 -0300

    changing execution files

--- build/mis/bin/instrumenter.bat
+++ build/mis/bin/instrumenter.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-SET CP="%MIS_HOME%\libs\commons.jar"
-SET CP="%MIS_HOME%\libs\inss.jar"
-SET CP=%CP%;"%MIS_HOME%\libs\jabuti.jar"
-SET CP=%CP%;"%MIS_HOME%\libs\bcel-5.2.jar"
-SET CP=%CP%;"%MIS_HOME%\libs\commons-cli-1.2.jar"
-SET CP=%CP%;"%MIS_HOME%\libs\log4j-1.2.16.jar"
+SET CP="%INSS_HOME%\libs\commons.jar"
+SET CP="%INSS_HOME%\libs\inss.jar"
+SET CP=%CP%;"%INSS_HOME%\libs\jabuti.jar"
+SET CP=%CP%;"%INSS_HOME%\libs\bcel-5.2.jar"
+SET CP=%CP%;"%INSS_HOME%\libs\commons-cli-1.2.jar"
+SET CP=%CP%;"%INSS_HOME%\libs\log4j-1.2.16.jar"
 java -cp %CP% br.usp.each.inss.bytecode.Instrumenter %*
